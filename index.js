a3b4f94210daede45661242f369b92259fd8cf4f
guantai <jpguantai@gmail.com>
2022-11-26 10:57:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 
-// let input = document.getElementById("button");
-
-// function addingEventListener() {
-
-//     alert("I was clciked!");
-    
-// }
-
-
 document .getElementById('button').addEventListener("click",function(){
     alert("I was clciked!");
 });
